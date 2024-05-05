b9eb43b9a84dd03e30cfa34b88d71848d6aeb791
Alejandro Uriarte <alejandro.uriartepatsi@gmail.com>
2024-05-05 10:54:44 -0400

    Created Patient and PatientManager clases

---
+++ PracticeTwo/businessLogic/Managers/PatientManager.cs
@@ -0,0 +1,63 @@
+﻿using PracticeTwo.BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTwo.BusinessLogic.Managers
+{
+    public class PatientManager
+    {
+        private List<Patient> _patients;
+
+        public PatientManager()
+        {
+            _patients = new List<Patient>();
+
+            _patients.Add(new Patient("Alejandro", "Uriarte", 123456));
+        }
+
+        public void AddPatient(string name, string lastname, int ci)
+        {
+            _patients.Add(new Patient(name, lastname, ci));
+        }
+
+        public void UpdatePatientByCI(int ci, string newName, string newLastname)
+        {
+            Patient patient = _patients.FirstOrDefault(p => p.CI == ci);
+            if (patient != null)
+            {
+                patient.Name = newName;
+                patient.Lastname = newLastname;
+            }
+            else
+            {
+                throw new InvalidOperationException("Patient not found");
+            }
+        }
+
+        public void DeletePatientByCI(int ci)
+        {
+            Patient patient = _patients.FirstOrDefault(p => p.CI == ci);
+            if (patient != null)
+            {
+                _patients.Remove(patient);
+            }
+            else
+            {
+                throw new InvalidOperationException("Patient not found");
+            }
+        }
+
+        public List<Patient> GetPatients()
+        {
+            return _patients;
+        }
+
+        public Patient GetPatientByCI(int ci)
+        {
+            return _patients.FirstOrDefault(p => p.CI == ci);
+        }
+    }
+}
---
+++ PracticeTwo/businessLogic/Models/Patient.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTwo.BusinessLogic.Models
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int CI { get; set; }
+
+        public string Bloodtype { get; set; }
+        public Patient(string name, string lastname, int ci)
+        {
+            Name = name;
+            Lastname = lastname;
+            CI = ci;
+            Bloodtype = GenerateRandomBloodtype();
+        }
+
+        private string GenerateRandomBloodtype()
+        {
+            Random rnd = new Random();
+            int numeroAleatorio = rnd.Next(1, 9);
+            string randomBloodtype = "";
+            switch (numeroAleatorio)
+            {
+                case 1:
+                    randomBloodtype = "O-";
+                    break;
+                case 2:
+                    randomBloodtype = "O+";
+                    break;
+                case 3:
+                    randomBloodtype = "A-";
+                    break;
+                case 4:
+                    randomBloodtype = "A+";
+                    break;
+                case 5:
+                    randomBloodtype = "B-";
+                    break;
+                case 6:
+                    randomBloodtype = "B+";
+                    break;
+                case 7:
+                    randomBloodtype = "AB-";
+                    break;
+                case 8:
+                    randomBloodtype = "AB+";
+                    break;
+            }
+
+            return randomBloodtype;
+        }
+    }
+}
